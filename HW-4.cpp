230e349fdefafbd70dc7a0daa7e50d3a03093e5f
maede1382 <152260037+maede1382@users.noreply.github.com>
2023-11-28 11:17:40 +0330

    Add files via upload

---
+++ HW-4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+ main () {
+
+float m , n , A , S , K , R ;
+
+cout << "please enter 2 decimal number" << "\n" << ":" ;
+
+cin >> m ;
+
+cout << ":" ;
+
+cin >> n ;
+
+A = m + n ;
+
+S = m - n ;
+
+K = m * n ;
+
+R = m / n ;
+
+cout << "plus=" << A << "\n" << "minus=" << S << "\n" << "multiplay=" << K << "\n" << "divided by" << R ;
+
+}
