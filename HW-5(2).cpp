df4e9b4ab83a81e07ba785c7b7b637b98260ef7b
maede1382 <152260037+maede1382@users.noreply.github.com>
2023-12-09 22:02:53 +0330

    Add files via upload

---
+++ HW-5(2).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std ;
+
+ main() {
+ 	
+ 	char  vowel ;
+ 	
+ 	cout << " please enter a character " ;
+ 	
+ 	cin >> vowel ;
+ 	
+ 	switch ( vowel ) {
+ 		
+	 case 'a' :
+	 	 cout << " vowel " ;
+ 		     break ;
+ 		
+ 	case 'e' :
+ 		cout << " vowel " ;
+	    	break ;
+		
+	case 'u' :
+		cout << " vowel " ;
+	        break ;	
+		
+	case 'o' :
+	   	 cout << " vowel " ;
+	    	 break ;
+	   	 
+	case 'i' :
+	   	cout << " vowel " ;    
+		    
+		 
+	 }
+ 	
+ 		
+
+ 							
+ 							
+	 }
+	 
+	
+
